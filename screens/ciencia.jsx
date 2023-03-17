db4dffada7181c150be03093f7ddf66913e4ca4e
Telmo J Moura <telmop1999@gmail.com>
2023-03-17 15:15:12 +0000

    loading done

--- screens/ciencia.jsx
+++ screens/ciencia.jsx
@@ -6,6 +6,7 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
+  SafeAreaView,
 } from "react-native";
 
 import { globalStyles } from "../styles/global";
@@ -13,38 +14,59 @@ import { APP_NAME, API_TOKEN, API_KEY } from "@env";
 
 import HC008 from "../shared/HC008";
 
-export default function Ciencia({navigation}) {
+export default function Ciencia({ navigation }) {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const getAll = async () => {
-    const allPost = await fetch(
-      `https://posts2-api.global.ssl.fastly.net/1/dossier/6/posts?apikey=${API_KEY}&apitoken=${API_TOKEN}&include=bodies,tags,photos,albums,authors,labels,audios,documents,dossiers,collections&filter[isoLanguage]=pt`
-    );
+    setIsLoading(true);
 
-    const data = await allPost.json();
+    try {
+      const allPost = await fetch(
+        `https://posts2-api.global.ssl.fastly.net/1/dossier/6/posts?apikey=${API_KEY}&apitoken=${API_TOKEN}&include=bodies,tags,photos,albums,authors,labels,audios,documents,dossiers,collections&filter[isoLanguage]=pt`
+      );
 
-    setPosts(data);
+      const data = await allPost.json();
+      setIsLoading(false);
+
+      setPosts(data);
+    } catch (error) {
+      setError(error);
+    }
   };
 
-  useEffect(()=>{
-    getAll()
- },[])
+  useEffect(() => {
+    getAll();
+  }, []);
   return (
-    <ScrollView style={globalStyles.skContainer}>
-      <Text style={globalStyles.titleCategory}>Ciência e Tecnologia</Text>
-      {posts?.data?.map((post, index) => (
-        <View key={post.l10n[0].id}>
-          <HC008
-            img={post.baseUrl + "/" + post.l10n[0].image}
-            category={post.dossiers[0].managementName}
-            title={post.l10n[0].title}
-            date={post.l10n[0].publishedAt}
-            readTime={post.l10n[0].readTime}
-            onPress={() => navigation.navigate("openArticle", {post:post})}
-          />
-        </View>
-      ))}
-    </ScrollView>
+    <SafeAreaView>
+      {isLoading ? (
+        <ActivityIndicator
+          style={globalStyles.loading}
+          size="large"
+          color="#00ff00"
+        />
+      ) : (
+        <ScrollView style={globalStyles.skContainer}>
+          <Text style={globalStyles.titleCategory}>Ciência e Tecnologia</Text>
+          {posts?.data?.map((post, index) => (
+            <View key={post.l10n[0].id}>
+              <HC008
+                img={post.baseUrl + "/" + post.l10n[0].image}
+                category={post.dossiers[0].managementName}
+                title={post.l10n[0].title}
+                date={post.l10n[0].publishedAt}
+                readTime={post.l10n[0].readTime}
+                onPress={() =>
+                  navigation.navigate("openArticle", { post: post })
+                }
+              />
+            </View>
+          ))}
+        </ScrollView>
+      )}
+    </SafeAreaView>
   );
 }
 
--- screens/cultura.jsx
+++ screens/cultura.jsx
@@ -6,6 +6,7 @@ import {
   Image,
   TouchableOpacity,
   ScrollView,
+  SafeAreaView,
 } from "react-native";
 
 import { globalStyles } from "../styles/global";
@@ -13,41 +14,60 @@ import { APP_NAME, API_TOKEN, API_KEY } from "@env";
 
 import HC008 from "../shared/HC008";
 
-export default function Cultura({navigation}) {
+export default function Cultura({ navigation }) {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
   const getAll = async () => {
     setIsLoading(true);
-    const allPost = await fetch(
-      `https://posts2-api.global.ssl.fastly.net/1/dossier/1/posts?apikey=${API_KEY}&apitoken=${API_TOKEN}&include=bodies,tags,photos,albums,authors,labels,audios,documents,dossiers,collections&filter[isoLanguage]=pt`
-    );
 
-    const data = await allPost.json();
+    try {
+      const allPost = await fetch(
+        `https://posts2-api.global.ssl.fastly.net/1/dossier/1/posts?apikey=${API_KEY}&apitoken=${API_TOKEN}&include=bodies,tags,photos,albums,authors,labels,audios,documents,dossiers,collections&filter[isoLanguage]=pt`
+      );
 
-    setPosts(data);
+      const data = await allPost.json();
+
+      setIsLoading(false);
+
+      setPosts(data);
+    } catch (error) {
+      setError(error);
+    }
   };
 
   useEffect(() => {
     getAll();
   });
   return (
-    <ScrollView style={globalStyles.skContainer}>
-      <Text style={globalStyles.titleCategory}>Cultura</Text>
-      {posts?.data?.map((post, index) => (
-        <View key={post.l10n[0].id}>
-          <HC008
-            img={post.baseUrl + "/" + post.l10n[0].image}
-            category={post.dossiers[0].managementName}
-            title={post.l10n[0].title}
-            date={post.l10n[0].publishedAt}
-            readTime={post.l10n[0].readTime}
-            onPress={() => navigation.navigate("openArticle", {post:post})}
-          />
-        </View>
-      ))}
-    </ScrollView>
+    <SafeAreaView>
+      {isLoading ? (
+        <ActivityIndicator
+          style={globalStyles.loading}
+          size="large"
+          color="#00ff00"
+        />
+      ) : (
+        <ScrollView style={globalStyles.skContainer}>
+          <Text style={globalStyles.titleCategory}>Cultura</Text>
+          {posts?.data?.map((post, index) => (
+            <View key={post.l10n[0].id}>
+              <HC008
+                img={post.baseUrl + "/" + post.l10n[0].image}
+                category={post.dossiers[0].managementName}
+                title={post.l10n[0].title}
+                date={post.l10n[0].publishedAt}
+                readTime={post.l10n[0].readTime}
+                onPress={() =>
+                  navigation.navigate("openArticle", { post: post })
+                }
+              />
+            </View>
+          ))}
+        </ScrollView>
+      )}
+    </SafeAreaView>
   );
 }
 
